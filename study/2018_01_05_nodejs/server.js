76d3bd47823724eaae7f385d00aa640422e8f85b
eunjeong Jang <cs801458j@gmail.com>
2018-01-05 17:12:49 -0800

    add server running code

--- study/2018_01_05_nodejs/server.js
+++ study/2018_01_05_nodejs/server.js
@@ -1,5 +1,5 @@
 //  Activating Server
-
+/*
 var http = require('http');
 
 http.createServer(function (request, response){
@@ -7,3 +7,17 @@ http.createServer(function (request, response){
     response.write("Hello World!");
     response.end();
 }).listen(8888);
+*/
+
+var http = require('http');
+//  요청을 하고 응답을 받는다.
+function onRequest(request, response){
+    console.log("사용자가 request 합니다"+ request.url);
+    //  200: 정상적인 접속 번호
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("This is server response(data...)");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server is running");
